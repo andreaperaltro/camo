ef95edec60e3cbb0e760d32e96a5768ab0dea3cd
Your Name <your.email@example.com>
2025-05-04 14:27:04 +0200

    Fix Next.js and Vercel configuration

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Basic settings to make it work with Vercel
-  swcMinify: true,
-  trailingSlash: false,
-  // We're not using static export for Vercel
-  // output: 'export',
   images: {
     domains: ['localhost', 'camo-generator.vercel.app'],
-    // Don't set unoptimized when deploying to Vercel
-    // unoptimized: true,
+    unoptimized: true,
   }
 }
 
